67050fab0cd27ddcb1352d123abd4fd0886c521a
frussian <zoomod57@gmail.com>
2022-10-06 18:00:55 +0300

    updated client and cfg header

--- esp32_client/esp32_client.ino
+++ esp32_client/esp32_client.ino
@@ -10,15 +10,17 @@
 #include "esp32_setup.h"
 #include "wifi_setup.h"
 
-IPAddress server_ip(185, 188, 183, 121);
-int server_port = 8080;
-
 WiFiClient wifi_client;
 PubSubClient client(server_ip, server_port, wifi_client);
 
 String camera_topic = String("cameras/") + String(camera_name);
 String camera_state_topic = camera_topic + String("/state");
 String camera_data_topic = camera_topic + String("/data");
+String camera_cmd_topic = camera_topic + String("/cmd");
+
+unsigned long last_dump_msec = 0;
+//use qos2 to send exactly 1 stream request to camera
+int dump_camera_subscribers = 0;
 
 //https://github.com/knolleary/pubsubclient/issues/791#issuecomment-800096719
 void publish_large_mqtt(char* channel, uint8_t *data, uint32_t len) {
@@ -46,75 +48,97 @@ void publish_large_mqtt(char* channel, uint8_t *data, uint32_t len) {
   Serial.printf("Published in MQTT channel %s: (binary data of length %d bytes, %d bytes written in %ld ms)\n", channel, len, len-to_write, millis()-start_ts);
 }
 
-void print_bytes(char *buf) {
-  int n = strlen(buf);
-  for (int i = 0; i < n; i++) {
+int dump_camera() {
+	camera_fb_t* fb = esp_camera_fb_get();
+	Serial.printf("camera handle: %p\n", fb);
+	if (fb == NULL) {
+	  return -1;
+	}
+	Serial.println(fb->format);
+	Serial.printf("%dx%d len: %d\n", fb->width, fb->height, fb->len);
+	publish_large_mqtt((char*)camera_data_topic.c_str(), fb->buf, fb->len);
+	esp_camera_fb_return(fb);
+
+	return 0;
+}
+
+void print_bytes(unsigned char *buf, int len) {
+  for (int i = 0; i < len; i++) {
     Serial.printf("%x ", buf[i]);
   }
   Serial.println();
 }
 
-void callback(char* topic, byte* payload, unsigned int length) {
-  // handle message arrived
-  //Serial.println(String(payload));
-  int r;
-  if (strcmp(topic, "camera/dump") == 0) {
-    camera_fb_t* fb = esp_camera_fb_get();
-    Serial.printf("camera handle: %p\n", fb);
-    if (fb == NULL) {
-      return;
-    }
-    Serial.println(fb->format);
-    Serial.printf("%dx%d len: %d\n", fb->width, fb->height, fb->len);
-    publish_large_mqtt((char*)camera_data_topic.c_str(), fb->buf, fb->len);
-    // r = client.beginPublish(camera_data_topic.c_str(), 128, false);
-    // Serial.println(r);
-    // r = client.write(fb->buf, 128);
-    // Serial.println(r);
-    // //client.write(fb->buf+60000, fb->len-60000);
-    // r = client.endPublish();
-    // Serial.println(r);
-    esp_camera_fb_return(fb);
-  }
+bool is_cmd(char *topic, byte *payload, unsigned int len, char *cmd) {
+	return strcmp(topic, camera_cmd_topic.c_str()) == 0 &&
+		len == strlen(cmd) && strncmp(payload, cmd, len);
+}
 
-  // Serial.println(topic);
-  print_bytes(topic);
-  Serial.println(String("received msg in topic ") + String(topic) + " len " + String(length));
+void callback(char* topic, byte* payload, unsigned int length) {
+	int r;
+
+	Serial.println(String("received msg in topic ") + String(topic) + " len " + String(length));
+	print_bytes(payload, length);
+	Serial.println();
+
+  	if (is_cmd(topic, payload, length, "req")) {
+    	dump_camera_subscribers++;
+	} else if (is_cmd(topic, payload, length, "stop")) {
+		dump_camera_subscribers--;
+		if (dump_camera_subscribers < 0) {
+			dump_camera_subscribers = 0;
+		}
+	} else if (is_cmd(topic, payload, length, "dump")) {
+		dump_camera();
+	}
+
+  	// Serial.println(topic);
 }
 
 bool connect_to_server() {
-  bool connected = client.connect(camera_name, camera_state_topic.c_str(), MQTTQOS1, true, "offline");
-  if (connected) {
-    Serial.println("connected to server");
-    const char *ptr = camera_state_topic.c_str();
-    Serial.printf("ptr %p\n", ptr);
-    client.publish(camera_state_topic.c_str(), "online", true);
-    client.subscribe("inTopic");
-    client.subscribe("camera/dump");
-  } else {
-    Serial.printf("cannot connect to server\n");
-  }
-  Serial.printf("client state %d\n", client.state());
-  return connected;
+	bool connected = client.connect(camera_name, username, password, camera_state_topic.c_str(), MQTTQOS1, true, "offline");
+	if (connected) {
+    	Serial.println("connected to server");
+    	const char *ptr = camera_state_topic.c_str();
+    	Serial.printf("ptr %p\n", ptr);
+		client.subscribe(camera_cmd_topic.c_str());
+    	client.publish(camera_state_topic.c_str(), "online", true);
+	} else {
+		Serial.printf("cannot connect to server\n");
+	}
+
+	Serial.printf("client state %d\n", client.state());
+	return connected;
 }
 
 void setup() {
-  int r;
+	int r;
 
-  setup_esp32();
-  //ssid and pass in module_info.ino
-  setup_wifi(ssid, pass);
+	setup_esp32();
+	//ssid and pass in module_info.ino
+	setup_wifi(ssid, pass);
 
-  client.setBufferSize(65535);
-  client.setCallback(callback);
+	client.setBufferSize(65535);
+	client.setCallback(callback);
 
-  connect_to_server();
+	connect_to_server();
 }
 
 void loop() {
-  if (!client.connected()) {
-    connect_to_server();
-  }
-  client.loop();
-  delay(300);
+	int r;
+
+	if (!client.connected()) {
+		r = client.state();
+		Serial.printf("mqtt client state: %d\n", r);;
+		connect_to_server();
+	}
+
+	client.loop();
+
+	unsigned long curr_msec = millis();
+	if (dump_camera_subscribers && curr_msec - last_dump_msec > dump_one_frame_msec) {
+		last_dump_msec = curr_msec;
+		dump_camera();
+	}
+	// delay(300);
 }
